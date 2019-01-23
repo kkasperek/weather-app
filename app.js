36cbf6069368c623e59eb31bae94de2c3117ccd5
kkasperek <kt.kasperek@gmail.com>
2019-01-23 12:20:10 -0600

    api data added

--- app.js
+++ app.js
@@ -1,17 +1,36 @@
-window.addEventListener('load', ()=> {
-    let long;
-    let lat;
+// when the page loads
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  let timezone = document.querySelector('.location-timezone');
+  let tempDescription = document.querySelector('.temp-description');
+  let tempDegree = document.querySelector('.temp-degree');
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
-
-            const api = `https://api.darksky.net/forecast/${apikey}/${lat},${long}`;
-            
-        });
-    } else {
-        console.log("location unknown");
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position);
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+    
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/${apikey}/${lat},${long}`;
+     
+      // API call and return data as json
+      fetch(api)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+            console.log(data);
+            const { temperature, summary } = data.currently;
+            //set DOM elements from the API
+            tempDegree.textContent = temperature;
+            tempDescription.textContent = summary;
+            timezone.textContent = data.timezone;
+        })
+        .catch(error => console.error(error));
+    });
+  } else {
+    console.log("location unknown");
+  }
 });
